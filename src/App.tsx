06fb83c8aad4b515bc14dbbd4ae9a2341a6f612f
zhoukekestar <zhoukekestar@163.com>
2026-02-23 18:57:58 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,8 @@ function App() {
           }
         },
         // 使用 zstatic.net 国内 CDN 加速，替代默认的 cdn.jsdelivr.net
-        workerPath: './worker.min.js',
-        corePath: './',
+        // workerPath: './worker.min.js',
+        // corePath: './',
         // langPath: './',
       });
 
